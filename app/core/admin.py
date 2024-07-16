b477efcb11a981a737ecaf4db8f2c8e3e2954714
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-07-16 15:44:23 +0100

    Added functionality to create a new user from the Django Admin page.

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,13 +15,11 @@ class UserAdmin(BaseUserAdmin):
     ordering = ["id"]
     list_display = ["email", "name"]
     fieldsets = (
-        (None, {'fields':
-                    (
-                        'email',
-                        'password',
-                    )}),
-        (translate('Permissions'), {'fields':
-                                        (
+        (None,                      {'fields': (
+                                            'email',
+                                            'password',
+                                        )}),
+        (translate('Permissions'), {'fields': (
                                             'is_active',
                                             'is_superuser',
                                             'is_staff',
@@ -32,7 +30,20 @@ class UserAdmin(BaseUserAdmin):
                                         )}),
     )
     readonly_fields = ['last_login']
-
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_superuser',
+                'is_staff'
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -43,4 +43,13 @@ class AdminSiteTests(TestCase):
         url = reverse("admin:core_user_change", args=[self.user.id])
         res = self.client.get(url)
 
+        # Assertions
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test create user page from Django Admin Page."""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        # Assertions
         self.assertEqual(res.status_code, 200)
