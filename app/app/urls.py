58f7e841b5400757d6041cbb380a9274bb8257d9
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-08-20 16:36:26 +0100

    Fix - Fixed Linting error
    - Fixed 'SpectacularAPIView' and 'SpectacularSwaggerView' linting error for which GitHub Actions had failed in Pull Request from dev2 branch to main branch.

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,10 +19,6 @@ from django.urls import path, include
 from drf_spectacular.views import (SpectacularAPIView,
                                    SpectacularSwaggerView)
 
-
-from drf_spectacular.views import (SpectacularAPIView,
-                                   SpectacularSwaggerView)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema', SpectacularAPIView.as_view(), name='api-schema'),
