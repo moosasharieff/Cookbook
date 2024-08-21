82640258d37b253a8771ab75427fa2fa95b8ae57
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-08-21 12:15:35 +0100

    Feat, Test, Fix - TDD | Authorized Ingredient API Calls for listing ingredient succeed as expected and fixed create_user() function - Created a test case in cls: PrivateTestsIngredientAPI() to confirm if we are able to list the ingredients of a particular user. - Fixed method calling error in funtion: create_user(). - GitHub Actions locally passed -> Testing and Linting.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -1,13 +1,25 @@
 
-from django.contrib.auth import get_user_model # noqa
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from core.models import Ingredient
+
+from ..serializers import IngredientSerializer
+
 
 INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+def create_user(email: str, password: str) -> get_user_model:
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+def create_ingredient(user: str, name: str) -> Ingredient:
+    return Ingredient.objects.create(user=user, name=name)
 
 
 class PublicTestsIngredientAPI(TestCase):
@@ -25,3 +37,41 @@ class PublicTestsIngredientAPI(TestCase):
 
         # Assertions
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTestsIngredientAPI(TestCase):
+    """User authorized test cases.
+    # Possible API test cases on CRUD
+    0. C -> Create an ingredient
+    1. R -> Read list of ingredients of a user
+    2. U -> Update a particular ingredient of a user
+    3. D -> Delete a particular ingredient of a user
+    """
+
+    def setUp(self):
+        """Setting up test environment."""
+        # Create user directly in db
+        self.user = create_user('test@example.com', 'password@123')
+        # Instantiate Test Client
+        self.client = APIClient()
+        # Authorized user
+        self.client.force_authenticate(self.user)
+
+    def test_list_ingredients(self):
+        """Test returning list of ingredient for authenticated users."""
+        # Create ingredient directly in db
+        ingredient = create_ingredient(user=self.user, name='Potato')
+
+        # HTTP Request
+        res = self.client.get(INGREDIENT_URL)
+
+        # Serialize JSON Data with Db Data
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serialized = IngredientSerializer(ingredients, many=True)
+
+        # Assertions
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(serialized.data, res.data)
+        self.assertEqual(res.data[0]['id'], ingredient.id)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+
